b1afb8096724b4c8748253e867b74c3cd14b42ca
Lucca Nunes <lucca.nunes@ifood.com.br>
2024-11-05 16:33:29 -0300

    change output logic

--- algoritmos/maxdegree.cpp
+++ algoritmos/maxdegree.cpp
@@ -1,13 +1,15 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 #define pb push_back
 using namespace std;
 typedef vector<int> vi;
 typedef vector<vi> graph;
 typedef pair<int, int> pii;
 
-vector<pii> max_degree(graph G){
+vector<pii> max_degree(graph G)
+{
     vector<pii> v;
-    for (int i = 0; i < G.size(); i++){
+    for (int i = 0; i < G.size(); i++)
+    {
         v.pb({G[i].size(), i});
     }
     sort(v.begin(), v.end());
@@ -16,31 +18,27 @@ vector<pii> max_degree(graph G){
     return v;
 }
 
-void solve(){
-    int n, m; cin >> n >> m;
-    
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
     graph G(n);
-    for(int i = 0; i < m; i++){
-        int a, b; cin >> a >> b;
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
         G[a].push_back(b);
     }
-    
-    vector<pii> deg = max_degree(G);
 
-    ofstream file("degree_list");
-    if (file.is_open()) {
-        for(auto x : deg){
-            file << x.second << ' ';
-        }
-        file.close();
-    } else {
-        cout << "Failed to create the file.\n";
-    }
+    vector<pii> deg = max_degree(G);
+    for (auto x : deg)
+        cout << x.second << '\n';
 }
 
-
-int main() {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);	
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     solve();
     return 0;
 }
