d95cff8d97aad061e0988c1b25355751131b9ba3
Lucca Nunes <lucca.nunes@ifood.com.br>
2024-11-07 18:10:25 -0300

    altera formato dos arquivos de algoritmos

--- algoritmos_competicao/betweenness.cpp
+++ algoritmos_competicao/betweenness.cpp
@@ -13,24 +13,19 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<vi> graph;
 
-const int maxn = 1e3+5, inf = 2e9, M = 1e9 + 7;
-const ll linf = 1e18;
-graph G;
-
-vi betweenness(graph G, int k, vi S){
-    ifstream file("./../algoritmos/betweenness_list");
+vi betweenness(graph G, int k, vi S)
+{
+    ifstream file("./../algoritmos/list-1613/betweenness_list.txt");
     int a;
     vi ans;
-    while (file >> a) {
+    while (file >> a)
+    {
         auto it = find(all(S), a);
-        if(it == S.end()) ans.pb(a);
-        if(ans.size() == k) break;
+        if (it == S.end())
+            ans.pb(a);
+        if (ans.size() == k)
+            break;
     }
 
     return ans;
-}
- 
-int main() {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    return 0;
 }
