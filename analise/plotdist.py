33450a04be786e28c7123cd3ea1479800852e51c
Yvens <yvensianpporto@gmail.com>
2024-11-09 21:30:06 -0300

    finish dist

--- analise/plotdist.py
+++ analise/plotdist.py
@@ -3,17 +3,23 @@ import numpy as np
 from matplotlib.ticker import MaxNLocator
 import sys
 data = []
+mean = 0
+tot = 0
 for line in sys.stdin:
     line = line.strip()
     if(line != ''):
         line = int(line)
         if(line < 15000 and line != 0):
+            tot += 1
+            mean += line
             data.append(int(line))  # Parse numbers as they come from C++
 # print(data[:10])  # Print first 10 numbers as an example
 
+print(mean, tot)
+
 data = np.array(data)
 
-plt.hist(data, bins=20, edgecolor='black')
+plt.hist(data, bins=15, edgecolor='black')
 plt.xlabel('Valor')
 plt.ylabel('Frequência')
 plt.title('Distribuição de Distâncias entre dois Vértices')
