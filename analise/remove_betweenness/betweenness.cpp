a67f46e56b220ec366c8d581128c7135a53efef5
Yvens <yvensianpporto@gmail.com>
2024-11-11 17:19:35 -0300

    betweenness

--- analise/remove_betweenness/betweenness.cpp
+++ analise/remove_betweenness/betweenness.cpp
@@ -17,12 +17,14 @@ const int maxn = 1e3 + 5, inf = 2e9, M = 1e9 + 7;
 const ll linf = 1e18;
 graph G;
 vector<ld> betweenness;
-vector<ld> edge_betweenness;
-map<pair<int, int>, int> mapa;
+vector<pair<ld, pii>> edge_betweenness;
+map<pii, int> mapa;
+map<int, pii> unmp;
+map<pii, set<int>> marcados;
 int n, m, dif_m;
 
 // Brandes
-void bfs(int a, graph G)
+void bfs(int a, graph G, int aux)
 {
     stack<int> ordem;
 
@@ -75,14 +77,15 @@ void bfs(int a, graph G)
         {
             delta[y] += ((ld)qtdd[y] / qtdd[x]) * (1 + delta[x]);
             delta_edge[mapa[{y, x}]] += ((ld)qtdd[y] / qtdd[x]) * (1 + delta[x]);
+            marcados[{y, x}].insert(a);
         }
         if (x != a)
         {
-            betweenness[x] += delta[x];
+            betweenness[x] += aux * delta[x];
         }
     }
     for(int i = 0; i < dif_m; i++){
-        edge_betweenness[i] += delta_edge[i];
+        edge_betweenness[i].first += aux * delta_edge[i];
     }
 }
 
@@ -95,49 +98,54 @@ void solve()
     graph G(n);
     dif_m = 0;
 
-    vector<pii> vetor;
-
     for (int i = 0; i < m; i++)
     {
         int a, b;
         cin >> a >> b;
-        G[a].push_back(b);
         if(mapa.find({a, b}) == mapa.end()){
+            G[a].push_back(b);
             mapa[{a, b}] = dif_m;
-            vetor.push_back({a, b});
+            unmp[dif_m] = {a, b};
             dif_m ++;
         }
     }
     edge_betweenness.resize(dif_m);
+    for(int i = 0; i < dif_m; i++){
+        edge_betweenness[i].second = unmp[i];
+    }
 
     for (int i = 0; i < G.size(); i++)
     {
-        bfs(i, G);
+        bfs(i, G, +1);
     }
 
-    ofstream outfile("edge_betweenness");
+    sort(edge_betweenness.begin(), edge_betweenness.end());
 
-    for (int i = 0; i < edge_betweenness.size(); i++){
-        ld x = edge_betweenness[i];
-        outfile << x / (((ll)n) * (n - 1)) << '\n';
+
+    for(int i = 0; i < dif_m; i++){
+        auto x = (*max_element(edge_betweenness.begin(), edge_betweenness.end())).second;
+        cout << x.first << ' ' << x.second << '\n';
+
+        for(auto y : marcados[x]){
+            bfs(y, G, -1);
+        }
+
+        for(auto it = G[x.first].begin(); it != G[x.first].end(); it++){
+            if(*it == x.second) {
+                G[x.first].erase(it);
+                break;
+            }
+        }
+
+        for(auto y : marcados[x]){
+            bfs(y, G, +1);
+        }
     }
-
-    outfile << '\n';
-    outfile.close();
-
-    ofstream outfile2("node_betweenness");
-
-    for (auto x : betweenness){
-        outfile2 << x / (((ll)n - 1) * (n - 2))<< '\n';
-    }
-
-    outfile2 << '\n';
-    outfile2.close();
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    // ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     solve();
     return 0;
 }
