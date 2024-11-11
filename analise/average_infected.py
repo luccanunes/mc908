45cbc55eb649b1052d4b501f98d6694d2da28946
Lucca Nunes <lucca.nunes@ifood.com.br>
2024-11-10 23:15:39 -0300

    plota grafico de analise isolada

---
+++ analise/average_infected.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Ler os dados do arquivo CSV
+df = pd.read_csv('../simulador/algorithm_curves.csv')
+
+# Plotar o gráfico de curvas
+plt.figure(figsize=(10, 6))
+for alg in df['Algorithm'].unique():
+    subset = df[df['Algorithm'] == alg]
+    plt.plot(subset['InitialInfected'], subset['AverageInfected'], label=alg)
+
+plt.xlabel('Tamanho do Conjunto Inicial de Infectados')
+plt.ylabel('Quantidade Total de Infectados')
+plt.title('Curvas de Propagação de Influência por Algoritmo')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.savefig('algorithm_curves.png')
+plt.show()
