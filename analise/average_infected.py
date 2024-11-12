fb3f803d0128b383848205834f1334384d947cd5
Lucca Nunes <lucca.nunes@ifood.com.br>
2024-11-11 22:02:22 -0300

    add graphs

--- analise/average_infected.py
+++ analise/average_infected.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Ler os dados do arquivo CSV
+# df = pd.read_csv('../simulador/algorithm_curves_50steps_23min.csv')
 df = pd.read_csv('../simulador/algorithm_curves.csv')
 
 # Plotar o gráfico de curvas
@@ -12,7 +13,7 @@ for alg in df['Algorithm'].unique():
 
 plt.xlabel('Tamanho do Conjunto Inicial de Infectados')
 plt.ylabel('Quantidade Total de Infectados')
-plt.title('Curvas de Propagação de Influência por Algoritmo')
+plt.title('Curvas de Propagação de Influência por Algoritmo: 1 passo')
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
