05627c44f3d6aad9cb99d63e094e9e335b57d002
Yvens <yvensianpporto@gmail.com>
2024-11-16 20:25:27 -0300

    Comenta Autovetor

--- algoritmos/eigenvector.cpp
+++ algoritmos/eigenvector.cpp
@@ -1,24 +1,14 @@
 #include <bits/stdc++.h>
-#define pb push_back
-#define all(x) x.begin(), x.end()
-#define pc __builtin_popcount
-#define F first
-#define S second
-
 using namespace std;
 
-typedef long long int ll;
 typedef long double ld;
-typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<vi> graph;
 
-const int maxn = 1e3 + 5, inf = 2e9, M = 1e9 + 7;
-const ll linf = 1e18;
 graph G;
 vector<ld> eigenvector;
 
-// funciona somente se conexo
+// Calcula a norma de um vetor v
 vector<ld> norm(vector<ld> v)
 {
     ld sum = 0;
@@ -34,7 +24,7 @@ vector<ld> norm(vector<ld> v)
     return v;
 }
 
-// Von Mises
+// Algoritmo de Von Mises para autovetor
 void vonmises(graph G)
 {
     vector<ld> aux(G.size());
@@ -59,6 +49,7 @@ void solve()
     eigenvector.resize(n);
     eigenvector[0] = 1;
 
+    // Lê o grafo
     graph G(n);
     for (int i = 0; i < m; i++)
     {
@@ -67,12 +58,14 @@ void solve()
         G[a].push_back(b);
     }
 
+    // 100 iterações de VonMises
     int times = 100;
     for (int i = 0; i < times; i++)
     {
         vonmises(G);
     }
 
+    // Imprime vértices em ordem de autovalor
     vector<pair<ld, int>> aux(G.size());
 
     for (int i = 0; i < G.size(); i++)
@@ -88,6 +81,7 @@ void solve()
         cout << x.second << '\n';
 }
 
+// Só funciona se conexo
 int main()
 {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
